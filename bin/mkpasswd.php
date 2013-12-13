b6faa7b9191e433b4eca7e2c3e71842616cc3f41
Tim Otten <totten@civicrm.org>
2013-12-13 14:00:28 -0800

    mkpasswd.php - Add missing dependency

---
+++ bin/mkpasswd.php
@@ -0,0 +1,17 @@
+#!/usr/bin/env php
+<?
+function createRandom($len, $alphabet) {
+  $alphabetSize = strlen($alphabet);
+  $result = '';
+  for ($i = 0; $i < $len; $i++) {
+    $result .= $alphabet{rand(1, $alphabetSize) - 1};
+  }
+  return $result;
+}
+
+if (empty($argv[1])) {
+  $len = 8;
+} else {
+  $len = $argv[1];
+}
+print createRandom($len, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
