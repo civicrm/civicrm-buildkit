d17583cb6e4c059c75f322313c208a74c34bbb64
Tim Otten <totten@civicrm.org>
2013-12-20 19:53:23 -0600

    Add hrdemo

---
+++ app/config/hrdemo/install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+## install.sh -- Create config files and databases; fill the databases
+
+###############################################################################
+## Create virtual-host and databases
+
+## "amp create" outputs variables, CMS_URL, CMS_DB_* and CIVI_DB_*
+if [ -n "$CMS_URL" ]; then
+  eval $(amp create -f --root="$WEB_ROOT" --name=cms --prefix=CMS_ --url="$CMS_URL")
+else
+  eval $(amp create -f --root="$WEB_ROOT" --name=cms --prefix=CMS_)
+fi
+eval $(amp create -f --root="$WEB_ROOT" --name=civi --prefix=CIVI_ --no-url)
+
+###############################################################################
+## Setup Drupal (config files, database tables)
+
+drupal_singlesite_install
+
+###############################################################################
+## Setup CiviCRM (config files, database tables)
+
+CIVI_CORE="${WEB_ROOT}/sites/all/modules/civicrm"
+CIVI_SETTINGS="${WEB_ROOT}/sites/default/civicrm.settings.php"
+CIVI_FILES="${WEB_ROOT}/sites/default/files/civicrm"
+CIVI_TEMPLATEC="${CIVI_FILES}/templates_c"
+CIVI_UF="Drupal"
+
+civicrm_install
+
+###############################################################################
+## Extra configuration
+
+drush -y updatedb
+drush -y dis overlay shortcut color
+drush -y en civicrm toolbar civicrmtheme
+drush -y vset theme_default seven
+drush -y vset civicrmtheme_theme_admin seven
+drush -y vset site_frontpage "civicrm/dashboard"
+
+drush -y en civicrm_webtest
+drush -y user-create --password="$DEMO_PASS" --mail="$DEMO_EMAIL" "$DEMO_USER"
+drush -y user-add-role civicrm_webtest_user "$DEMO_USER"
+
+bash ${CIVI_CORE}/tools/extensions/civihr/bin/drush-install.sh --with-sample-data
+drush php-eval '$roles=user_roles(); user_role_grant_permissions(array_search("civicrm_webtest_user", $roles), array("access HRJobs","edit HRJobs"));'
---
+++ app/config/hrdemo/uninstall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## uninstall.sh -- Delete config files and databases
+
+###############################################################################
+
+if [ -f "$CIVI_SETTINGS" ]; then
+  rm -f "$CIVI_SETTINGS"
+fi
+
+drupal_singlesite_uninstall
+
+## Disabled to provide continuity during rebuilds
+## Maybe we should a destroy function that preserves the old metadata?
+#amp destroy --root="$WEB_ROOT" --name=cms
+#amp destroy --root="$WEB_ROOT" --name=civi
