5a285bcb88b47f97ba25e37d9ae1736c527c966a
Tim Otten <totten@civicrm.org>
2014-01-03 14:22:13 -0800

    vagrant/precise32 - Add (rough) Vagrantfile for Ubuntu 12.04

---
+++ vagrant/precise32/bootstrap.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -e
+###############################################################
+## This script is executed when launching a new virtual machine
+###############################################################
+
+## Update apt
+sed -i 's/deb-src/#deb-src/' /etc/apt/sources.list
+apt-get update
+
+## Some basic helpers
+apt-get install -y wget curl joe makepasswd
+
+## MySQL
+MYSQLPASS=$(makepasswd --chars=16)
+echo "mysql-server-5.5 mysql-server/root_password password $MYSQLPASS" | debconf-set-selections
+echo "mysql-server-5.5 mysql-server/root_password_again password $MYSQLPASS" | debconf-set-selections
+apt-get install -y mysql-server-5.5 mysql-client-5.5
+cat > /root/.my.cnf << EOF
+[client]
+user=root
+password=MYSQLPASS
+EOF
+chmod 600 /root/.my.cnf
+cp /root/.my.cnf /home/vagrant/.my.cnf
+chown vagrant.vagrant /home/vagrant/.my.cnf
+
+## PHP
+apt-get install -y php5-{cli,imap,ldap,curl,mysql,intl,gd} php-apc
+
+## Apache
+apt-get install -y apache2 libapache2-mod-php5
+
+## civicrm-project -- note: code is already shared (via Vagrantfile)
+PRJDIR=/home/vagrant/civicrm-project
+cat > /etc/profile.d/civicrm_project.sh << EOF
+PATH="$PRJDIR/bin:\$PATH"
+export PATH
+EOF
+
+#[ ! -d "$PRJDIR/app/tmp/apache.d" ] && mkdir -p "$PRJDIR/app/tmp/apache.d"
+echo "Include /home/vagrant/.amp/apache.d/*.conf" > /etc/apache2/conf.d/civicrm-project
+
+sudo -u vagrant -H $PRJDIR/bin/amp config:set \
+  --mysql_type="dsn" \
+  --mysql_dsn="mysql://root:$MYSQLPASS@localhost:3306" \
+  --httpd_type="apache" \
+  --perm_type="none"
+#  --apache_dir="$PRJDIR/app/tmp/apache.d" \
