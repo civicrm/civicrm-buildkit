160247efe120e17328570782a3b8fc48e68c8b7e
Tim Otten <totten@civicrm.org>
2014-01-04 18:37:01 -0800

    drupal-demo/install.sh - Add comments/sections. Add setup for "userprotect"

--- app/config/drupal-demo/install.sh
+++ app/config/drupal-demo/install.sh
@@ -27,17 +27,29 @@ civicrm_install
 ## Extra configuration
 
 drush -y updatedb
-drush -y dis overlay shortcut color
-drush -y en civicrm toolbar garland login_destination
-drush -y vset theme_default garland
+drush -y en civicrm toolbar locale
 
+## Setup theme
+drush -y en garland
+drush -y vset theme_default garland
+echo 'update block set region="sidebar_first" where theme="garland" and module="user" and delta="login"' | drush sql-cli
+echo 'update block set region="sidebar_first" where theme="garland" and module="system" and delta="navigation"' | drush sql-cli
+
+## Setup welcome page
 drush -y scr "$SITE_CONFIG_DIR/node-welcome.php"
 drush -y vset site_frontpage "welcome"
+
+## Setup login_destination
+drush -y en login_destination
 drush -y scr "$SITE_CONFIG_DIR/login-destination.php"
 
-echo 'update block set region ="sidebar_first" where module="user" and delta="login" and theme="garland"' | drush sql-cli
-echo 'update block set region ="sidebar_first" where module="system" and delta="navigation" and theme="garland"' | drush sql-cli
+## Setup userprotect
+drush -y en userprotect
+for perm in "change own e-mail" "change own openid" "change own password" ; do
+  drush role-remove-perm "authenticated user" "$perm"
+done
 
+## Setup demo user
 drush -y en civicrm_webtest
 drush -y user-create --password="$DEMO_PASS" --mail="$DEMO_EMAIL" "$DEMO_USER"
 drush -y user-add-role civicrm_webtest_user "$DEMO_USER"
