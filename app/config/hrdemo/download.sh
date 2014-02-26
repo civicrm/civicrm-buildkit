a4a6a73fa57058e14cd4368eb5d6ea1bf9f3e2c8
Tim Otten <totten@civicrm.org>
2014-02-25 17:14:42 -0800

    app/config/hrdemo - Accept variable HR_VERSION for specifying the release of CiviHR

--- app/config/hrdemo/download.sh
+++ app/config/hrdemo/download.sh
@@ -8,12 +8,14 @@ git_cache_setup "https://github.com/civicrm/civihr.git" "$CACHE_DIR/civicrm/civi
 
 [ -z "$CMS_VERSION" ] && CMS_VERSION=7.x
 [ -z "$CIVI_VERSION" ] && CIVI_VERSION=4.4
+[ -z "$HR_VERSION" ] && HR_VERSION=master
 
 MAKEFILE="${TMPDIR}/${SITE_TYPE}.make"
 cat "$SITE_CONFIG_DIR/drush.make.tmpl" \
   | sed "s;%%CACHE_DIR%%;${CACHE_DIR};" \
   | sed "s;%%CIVI_VERSION%%;${CIVI_VERSION};" \
   | sed "s;%%CMS_VERSION%%;${CMS_VERSION};" \
+  | sed "s;%%HR_VERSION%%;${HR_VERSION};" \
   > "$MAKEFILE"
 
 drush -y make --working-copy "$MAKEFILE" "$WEB_ROOT"
