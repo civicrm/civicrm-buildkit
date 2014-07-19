93295c0802e00319dd73b71b6e5e3c6286c74c61
Adam Roses Wight <awight@wikimedia.org>
2014-07-19 14:50:08 -0700

    Download PHPMailer and Twig dependencies

--- app/config/wmff/download.sh
+++ app/config/wmff/download.sh
@@ -3,6 +3,8 @@ set +x
 git_cache_setup "https://gerrit.wikimedia.org/r/wikimedia/fundraising/crm.git" "$CACHE_DIR/wikimedia/fundraising/crm.git"
 git_cache_setup "https://gerrit.wikimedia.org/r/wikimedia/fundraising/crm/civicrm.git" "$CACHE_DIR/wikimedia/fundraising/civicrm.git"
 git_cache_setup "https://gerrit.wikimedia.org/r/wikimedia/fundraising/crm/drupal.git" "$CACHE_DIR/wikimedia/fundraising/drupal.git"
+git_cache_setup "https://gerrit.wikimedia.org/r/wikimedia/fundraising/phpmailer.git" "$CACHE_DIR/wikimedia/fundraising/phpmailer.git"
+git_cache_setup "https://gerrit.wikimedia.org/r/wikimedia/fundraising/twig.git" "$CACHE_DIR/wikimedia/fundraising/twig.git"
 set -x
 
 # git clone --recursive https://git.wikimedia.org/git/wikimedia/fundraising/crm.git  fundraising-crm
@@ -11,6 +13,9 @@ git clone "$CACHE_DIR/wikimedia/fundraising/crm.git" "$WEB_ROOT"
 pushd "$WEB_ROOT"
   git clone "$CACHE_DIR/wikimedia/fundraising/civicrm.git" civicrm
   git clone "$CACHE_DIR/wikimedia/fundraising/drupal.git" drupal
+
+  git clone "$CACHE_DIR/wikimedia/fundraising/phpmailer.git" phpmailer
+  git clone "$CACHE_DIR/wikimedia/fundraising/twig.git" twig
 
   ## Reset the .git/config to match remotes in .gitmodules
   git submodule sync
