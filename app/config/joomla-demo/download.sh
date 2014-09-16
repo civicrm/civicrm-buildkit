a57236c8a1b3612594a5316c7b532b1d5f936d37
Jeremy Proffitt <jeremy@mobiusnm.com>
2014-09-16 00:21:01 -0400

    added Joomla console, updated amp/git scan, modified joomla install scripts

--- app/config/joomla-demo/download.sh
+++ app/config/joomla-demo/download.sh
@@ -4,26 +4,23 @@
 
 ###############################################################################
 
-git_cache_setup "https://github.com/joomla/joomla-cms.git" "$CACHE_DIR/joomla/joomla-cms.git"
-git clone "$CACHE_DIR/joomla/joomla-cms.git" "$WEB_ROOT"
+## TODO: Checkout Civi's code...
+cvutil_mkdir "$PRIVATE_ROOT" "$PRIVATE_ROOT/src"
 
-[ -z "$CMS_VER" ] && CMS_VER=3.2.1
-pushd "$WEB_ROOT" >> /dev/null
-  git checkout "$CMS_VER"
+pushd "$PRIVATE_ROOT" >> /dev/null
+git clone ${CACHE_DIR}/civicrm/civicrm-joomla.git    -b "$CIVI_VERSION" src/civicrm
+git clone ${CACHE_DIR}/civicrm/civicrm-core.git      -b "$CIVI_VERSION" src/civicrm/admin/civicrm
+git clone ${CACHE_DIR}/civicrm/civicrm-packages.git  -b "$CIVI_VERSION" src/civicrm/admin/civicrm/packages
 
-  ## Submitted PR to include cli/install.php in core -- https://github.com/joomla/joomla-cms/pull/2764
-  ## For the moment, we need to add it ourselves
-  if [ ! -f "cli/install.php" ]; then
-    cp "$SITE_CONFIG_DIR/cli-install.php" "cli/install.php"
-  fi
+git_set_hooks civicrm-joomla      src/civicrm                      "../admin/civicrm/tools/scripts/git"
+git_set_hooks civicrm-core        src/civicrm/admin/civicrm                      "../tools/scripts/git"
+git_set_hooks civicrm-packages    src/civicrm/admin/civicrm/packages          "../../tools/scripts/git"
 
-  ## TODO: Checkout Civi's code...
-  #git clone ${CACHE_DIR}/civicrm/civicrm-joomla.git    -b "$CIVI_VERSION" path/to/checkout/to
-  #git clone ${CACHE_DIR}/civicrm/civicrm-core.git      -b "$CIVI_VERSION" path/to/checkout/to
-  #git clone ${CACHE_DIR}/civicrm/civicrm-packages.git  -b "$CIVI_VERSION" path/to/checkout/to/packages
-
-  #git_set_hooks civicrm-joomla      path/to/checkout/to          "../civicrm/tools/scripts/git"
-  #git_set_hooks civicrm-core        path/to/checkout/to          "../tools/scripts/git"
-  #git_set_hooks civicrm-packages    path/to/checkout/to/packages "../../tools/scripts/git"
-
+pushd src/civicrm/admin > /dev/null
+  #ln -s admin.civicrm.php civicrm.php
+  mv admin.civicrm.php civicrm.php
 popd >> /dev/null
+
+cvutil_mkdir "$WEB_ROOT"
+
+
--- app/config/joomla-demo/install.sh
+++ app/config/joomla-demo/install.sh
@@ -10,36 +10,96 @@ amp_install
 ###############################################################################
 ## Setup Joomla (config files, database tables)
 
-pushd "$WEB_ROOT" >> /dev/null
-  CMS_DB_HOSTPORT=$(cvutil_build_hostport "$CMS_DB_HOST" "$CMS_DB_PORT")
-  php cli/install.php \
-    --db-user="$CMS_DB_USER" \
-    --db-name="$CMS_DB_NAME" \
-    --db-host="$CMS_DB_HOSTPORT" \
-    --db-pass="$CMS_DB_PASS" \
-    --admin-user="$ADMIN_USER" \
-    --admin-pass="$ADMIN_PASS" \
-    --admin-email="$ADMIN_EMAIL" \
-    --offline
 
-  ## Joomla requires removal of "installation" directory, which mucks up git,
-  ## so we'll push them off to the side.
-  [ -d installation ] && mv installation .installation.bak
-  [ -d .git ]         && mv .git .git.bak
-popd >> /dev/null
+
+# pushd "$WEB_ROOT" >> /dev/null
+#   CMS_DB_HOSTPORT=$(cvutil_build_hostport "$CMS_DB_HOST" "$CMS_DB_PORT")
+#   php cli/install.php \
+#     --db-user="$CMS_DB_USER" \
+#     --db-name="$CMS_DB_NAME" \
+#     --db-host="$CMS_DB_HOSTPORT" \
+#     --db-pass="$CMS_DB_PASS" \
+#     --admin-user="$ADMIN_USER" \
+#     --admin-pass="$ADMIN_PASS" \
+#     --admin-email="$ADMIN_EMAIL" \
+#     --offline
+
+#   ## Joomla requires removal of "installation" directory, which mucks up git,
+#   ## so we'll push them off to the side.
+#   [ -d installation ] && mv installation .installation.bak
+#   [ -d .git ]         && mv .git .git.bak
+# popd >> /dev/null
+
+joomla site:create "$SITE_NAME" --www="$WEB_ROOT" --mysql="$CMS_DB_USER:$CMS_DB_PASS@$CMS_DB_HOST:$CMS_DB_PORT" --joomla="$CMS_VERSION" --dbname="$CMS_DB_NAME" --nousers
+
+# Create Admin User
+joomla user:create -b "$WEB_ROOT" --name="Demonstrators Anonymous Administrator" --user="$ADMIN_USER" --pass="$ADMIN_PASS" --email="$ADMIN_EMAIL" --group='super users'
+
+# Create Demo User
+joomla user:create -b "$WEB_ROOT" --name="Demo User" --user="$DEMO_USER" --pass="$DEMO_PASS" --email="$DEMO_EMAIL" --group=registered
 
 ###############################################################################
 ## Setup CiviCRM (config files, database tables)
+pushd "$WEB_ROOT" >> /dev/null
+  ## usage: cvutil_link <to> <from>
+  function cvutil_link() {
+    from="$2"
+    to="$1"
+    cvutil_mkdir $(dirname "$to")
+    pushd $(dirname "$to") >> /dev/null
+      # ln -s "$from" $(basename "$to")
+      mv "$from" $(basename "$to")
+    popd >> /dev/null
+  }
+  cvutil_link plugins/user/civicrm                   ../../src/civicrm/admin/plugins/civicrm
+  cvutil_link plugins/quickicon/civicrmicon          ../../src/civicrm/admin/plugins/civicrmicon
+  cvutil_link plugins/system/civicrmsys              ../../src/civicrm/admin/plugins/civicrmsys
+  cvutil_link administrator/components/com_civicrm   ../../src/civicrm/admin 
+  cvutil_link components/com_civicrm                    ../src/civicrm/site
+
+popd >> /dev/null
 
 CIVI_DOMAIN_NAME="Demonstrators Anonymous"
 CIVI_DOMAIN_EMAIL="\"Demonstrators Anonymous\" <info@example.org>"
-CIVI_CORE="${WEB_ROOT}/sites/all/modules/civicrm"
-CIVI_SETTINGS="${WEB_ROOT}/sites/default/civicrm.settings.php"
-CIVI_FILES="${WEB_ROOT}/sites/default/files/civicrm"
+CIVI_CORE="${WEB_ROOT}/${SITE_NAME}/administrator/components/com_civicrm/civicrm"
+CIVI_SETTINGS="${WEB_ROOT}/${SITE_NAME}/components/com_civicrm/civicrm.settings.php"
+CIVI_ADMSETTINGS="${WEB_ROOT}/${SITE_NAME}/administrator/components/com_civicrm/civicrm.settings.php"
+CIVI_FILES="${WEB_ROOT}/${SITE_NAME}/media/civicrm"
 CIVI_TEMPLATEC="${CIVI_FILES}/templates_c"
 CIVI_UF="Joomla"
 
-#civicrm_install
+cat > "$CIVI_CORE/civicrm.config.php" <<EOF
+<?php
+define('CIVICRM_JOOMLA_BASE', '$WEB_ROOT');
+define('CIVICRM_SETTINGS_PATH', '$CIVI_ADMSETTINGS');
+\$error = @include_once( '$CIVI_ADMSETTINGS' );
+if ( \$error == false ) {
+    echo "Could not load the settings file at: $CIVI_ADMSETTINGS\n";
+    exit( );
+}
+
+// Load class loader
+require_once \$civicrm_root . '/CRM/Core/ClassLoader.php';
+CRM_Core_ClassLoader::singleton()->register();
+EOF
+
+civicrm_install
+sed "s;$CMS_URL;$CMS_URL/administrator/;g" < "$CIVI_SETTINGS" > "$CIVI_ADMSETTINGS"
+## $CMS_URL/
+
+cvutil_mkdir "$TMPDIR/$SITE_NAME"{,/joomlaxml,/joomlaxml/admin}
+php "$CIVI_CORE/distmaker/utils/joomlaxml.php" "$CIVI_CORE" "$TMPDIR/$SITE_NAME/joomlaxml" "$CIVI_VERSION" alt
+cp -f "$TMPDIR/$SITE_NAME/joomlaxml/civicrm.xml" "$WEB_ROOT/administrator/components/com_civicrm/civicrm.xml"
+cp -f "$TMPDIR/$SITE_NAME/joomlaxml/admin/access.xml" "$WEB_ROOT/administrator/components/com_civicrm/access.xml"
+#echo '<?php /* AUTO-GENERATED */ ?>' > "administrator/components/com_civicrm/script.civicrm.php"
+cp -f "src/civicrm/script.civicrm.php" "administrator/components/com_civicrm/script.civicrm.php"
+
+#Only in joomla-demo.working-from-tarball/administrator/language/en-GB: en-GB.com_civicrm.ini
+#Only in joomla-demo.working-from-tarball/administrator/language/en-GB: en-GB.com_civicrm.sys.ini
+
+
+# Run Joomla Discover Install
+joomla extension:install "$SITE_NAME" civicrm --www="$WEB_ROOT" --mysql="$CMS_DB_USER":"$CMS_DB_PASS"@"$CMS_DB_HOSTPORT"
 
 set +x
 echo "================================================================================"
