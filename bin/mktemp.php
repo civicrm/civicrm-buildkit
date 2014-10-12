7add94157488e7d25338656bf5d631fffddb89df
Tim Otten <totten@civicrm.org>
2014-10-12 10:27:21 -0700

    mktemp.php - Add extra option for specifying the basedir

--- bin/mktemp.php
+++ bin/mktemp.php
@@ -1,3 +1,6 @@
 #!/usr/bin/env php
 <?php
-echo tempnam(sys_get_temp_dir(), empty($argv[1]) ? 'mktemp-' : $argv[1] );
+## usage: "mktemp.php [prefix [basedir]]"
+$prefix = empty($argv[1]) ? 'mktemp-' : $argv[1];
+$basedir = empty($argv[2]) ? sys_get_temp_dir() : $argv[2];
+echo tempnam($basedir,  $prefix);
