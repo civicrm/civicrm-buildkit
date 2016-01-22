c6b0036d41a6eb4eb90573fd689e67bf6a53a36d
Tim Otten <totten@civicrm.org>
2016-01-22 02:30:35 -0800

    CRM-17860 - Set DONT_DOCUMENT_TEST_CONFIG by default

---
+++ app/civicrm.settings.d/300-test-docs.php
@@ -0,0 +1,6 @@
+<?php
+// v4.6: Define()s for tests loaded via CiviTest/civicrm.settings.local.php
+// v4.7: Define()s for tests loaded via normal civicrm.setting.php
+if (!defined('DONT_DOCUMENT_TEST_CONFIG')) {
+  define('DONT_DOCUMENT_TEST_CONFIG', 1);
+}
