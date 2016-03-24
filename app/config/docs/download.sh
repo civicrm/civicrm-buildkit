cdd2b0ea5abd092662251f7da414b31625bf5319
Tim Otten <totten@civicrm.org>
2016-03-24 15:39:32 -0700

    app/config/docs/download.sh - Cleanup

--- app/config/docs/download.sh
+++ app/config/docs/download.sh
@@ -6,9 +6,9 @@
 
 echo "[[Check pre-requisites]]"
 if `which mkdocs` >> /dev/null ; then
-   echo "Found mkdocs"
+  echo "Found mkdocs"
 else
-  echo "Missing mkdocs. Please install http://www.mkdocs.org/"
+  echo "Missing mkdocs. Please install it globally (eg /usr/bin or /usr/local/bin). http://www.mkdocs.org/"
   exit 1
 fi
 
