5dc848451d3cdf6bde4efc378dc573e1a19def19
Tim Otten <totten@civicrm.org>
2016-06-16 16:46:55 -0700

    `civibuild download` - Fix when force downloading on top of D8

--- src/civibuild.app.sh
+++ src/civibuild.app.sh
@@ -121,6 +121,7 @@ function civibuild_app_download() {
 
   if [ -n "$FORCE_DOWNLOAD" -a -d "$WEB_ROOT" ]; then
     if cvutil_confirm "About to re-download \"$WEB_ROOT\". Are you sure you want destroy existing files? [y/N] " n y; then
+      chmod u+w -R "$WEB_ROOT"
       rm -rf "$WEB_ROOT"
     else
       echo "Aborted" 1>&2
