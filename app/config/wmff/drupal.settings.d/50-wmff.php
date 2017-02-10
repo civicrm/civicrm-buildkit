ea80e768bd3d746937ba6211f6f37087d44eb78a
Elliott Eggleston <ejegg@ejegg.com>
2017-02-09 23:17:56 -0500

    Update wmff settings
    Squashes these commits from gerrit.wikimedia.org:
    
    f582395 Update repos
    07482ae Enable new modules
    ac72e7d Fredge and Donations dbs are in Drupal schema for now
    e842d44 pushd to less crazy directory before running drush
    e056466 Don't install SimpleTest modules
    b10d085 Mock VCS revsion stamp
    2c092a4 Don't include civicrm_webtest
    18f321d We don't use the devel module in tests
    3338cb3 Need to composer install as part of testing
    1be39f9 Enable the AstroPay audit module
    1c3ae33 Activate the Amazon audit module
    889497e Prevent civicrm-buildkit from storing SQL dumps
    ea587fc Revert "Prevent civicrm-buildkit from storing SQL dumps"
    1d4d9bf Enable adyen_audit in wmff build
    efab0d2 Add banner history module to wmff setup
    c633915 Remove unused module
    2da0723 Use module list from crm repo
    0afedf8 Fix enabled modules path
    56c7d71 Add settings for donation-interface library
    6b26604 Stop failing tests on default country code
    
    Change-Id: I760115cdc7edbf44954e0a200140cc3b35e73990

--- app/config/wmff/drupal.settings.d/50-wmff.php
+++ app/config/wmff/drupal.settings.d/50-wmff.php
@@ -1,10 +1,6 @@
 <?php
 
-global $conf, $civibuild, $databases;
-
-$conf['wmf_common_di_location'] = $civibuild['WEB_ROOT'] . "/DonationInterface";
-$conf['wmf_common_phpmailer_location'] = $civibuild['WEB_ROOT'] . "/phpmailer";
-$conf['wmf_common_twig_location'] = $civibuild['WEB_ROOT'] . "/twig/current";
+global $databases;
 
 $databases['donations']['default'] = $databases['default']['default'];
 $databases['fredge']['default'] = $databases['default']['default'];
