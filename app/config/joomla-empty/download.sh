994c5e1f7d2b2d449eef023d837b7ee818e4f4e7
Tim Otten <totten@civicrm.org>
2017-06-27 19:41:37 -0700

    app/config/joomla-empty/download.sh - Joomla folder needs to be web writeable
    The installation process for Joomla extensions requires shuffling files --
    and it's not possible to install civicrm-joomla without making the folders
    writeable.

--- app/config/joomla-empty/download.sh
+++ app/config/joomla-empty/download.sh
@@ -6,6 +6,9 @@
 
 CMS_VERSION=${CMS_VERSION:-latest}
 
+## Joomla has strong expectation of writeable web-root -- eg can't run Civi installer otherwise. :(
+amp datadir "$WEB_ROOT"
+
 joomla site:download . --release="$CMS_VERSION" --www="$WEB_ROOT"
 
 #pushd "$WEB_ROOT" >> /dev/null
