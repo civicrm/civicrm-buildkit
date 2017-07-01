e4aad74fb06b357f7853bd1ad3ef142a15c38a5c
Tim Otten <totten@civicrm.org>
2017-06-30 19:53:45 -0700

    create-civicase-examples - Add example tags

--- src/create-civicase-examples.php
+++ src/create-civicase-examples.php
@@ -13,3 +13,13 @@ for ($i = 100; $i < 150; $i++) {
     'subject' => "The $i subject",
   ));
 }
+
+$fruit = civicrm_api3('Tag', 'create', array( 'name' => 'Fruit', 'description' => 'Sweet and nutritious', 'is_tagset' => 1, 'used_for' => array('civicrm_activity','civicrm_case')));
+civicrm_api3('Tag', 'create', array('name' => 'Apple', 'description' => 'An apple a day keeps the Windows away', 'color' => '#ec3737', 'parent_id' => $fruit['id']));
+civicrm_api3('Tag', 'create', array('name' => 'Banana', 'description' => 'Going bananas for tagsets', 'color' => '#d5d620', 'parent_id' => $fruit['id']));
+civicrm_api3('Tag', 'create', array('name' => 'Grape', 'description' => 'I heard it through the grapevine', 'color' => '#9044b8', 'parent_id' => $fruit['id']));
+civicrm_api3('Tag', 'create', array('name' => 'Orange', 'description' => 'Orange you glad this isn\'t a pun?', 'color' => '#ff9d2a', 'parent_id' => $fruit['id']));
+
+civicrm_api3('Tag', 'create', array( 'name' => 'Edge Case', 'description' => 'Edge Case', 'color' => '#000000', 'used_for' => array('civicrm_case')));
+civicrm_api3('Tag', 'create', array( 'name' => 'Strenuous', 'description' => 'Strenuous activity',  'color' => '#00ff00','used_for' => array('civicrm_activity')));
+civicrm_api3('Tag', 'create', array( 'name' => 'Leisurely', 'description' => 'Leisurely activity',  'color' => '#006f00','used_for' => array('civicrm_activity')));
