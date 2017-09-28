1fd53a56c5dc60ab30dba0a7c3b43d69c1c6c881
Kevin Cristiano <kcristiano@tadpole.cc>
2017-09-28 14:00:02 -0400

    Update wp-cli to v1.3
    Issue-321 softlinks fail on Windows Issue-359 specify WP Version explicitly or fall back to last major version
    
    Update wp-empty download script
    
    Update wp-empty download script

--- app/config/wp-empty/download.sh
+++ app/config/wp-empty/download.sh
@@ -5,14 +5,13 @@
 ###############################################################################
 [ -z "$VOL_VERSION" ] && VOL_VERSION='4.4-1.x'
 
-WPCLI_ARGS=
-[ -n "$CMS_VERSION" ] && WPCLI_ARGS="$WPCLI_ARGS --version=$CMS_VERSION"
+[ -z "$CMS_VERSION" ] && CMS_VERSION=4.8
 
 echo "[[Download WordPress]]"
 mkdir "$WEB_ROOT"
 pushd "$WEB_ROOT" >> /dev/null
-  "$PRJDIR/bin/wp" core download$WPCLI_ARGS
+  "$PRJDIR/bin/wp" core download --version=$CMS_VERSION
   if [ ! -e "wp-cli.yml" ]; then
-    ln -s "$SITE_CONFIG_DIR/wp-cli.yml" "wp-cli.yml"
+    cp -a "$SITE_CONFIG_DIR/wp-cli.yml" "wp-cli.yml"
   fi
 popd >> /dev/null
