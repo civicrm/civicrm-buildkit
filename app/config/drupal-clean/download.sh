1563a19bae30c17d71dadadf2a8babe1ee14b591
Tim Otten <totten@civicrm.org>
2017-12-15 19:00:30 -0700

    drupal-clean/download.sh - Small style tweaks

--- app/config/drupal-clean/download.sh
+++ app/config/drupal-clean/download.sh
@@ -15,13 +15,13 @@ pushd "$WEB_ROOT"
   drush dl -y libraries-1.0 views-3.7 devel
 
   pushd sites/all/modules
-    git clone ${CACHE_DIR}/civicrm/civicrm-core.git                     -b "$CIVI_VERSION"      civicrm
-    git clone ${CACHE_DIR}/civicrm/civicrm-drupal.git                   -b "7.x-$CIVI_VERSION"  civicrm/drupal
-    git clone ${CACHE_DIR}/civicrm/civicrm-packages.git                 -b "$CIVI_VERSION"      civicrm/packages
-    git clone ${CACHE_DIR}/eileenmcnaughton/civicrm_developer.git      -b master               civicrm_developer
+    git clone "${CACHE_DIR}/civicrm/civicrm-core.git"                        -b "$CIVI_VERSION"      civicrm
+    git clone "${CACHE_DIR}/civicrm/civicrm-drupal.git"                      -b "7.x-$CIVI_VERSION"  civicrm/drupal
+    git clone "${CACHE_DIR}/civicrm/civicrm-packages.git"                    -b "$CIVI_VERSION"      civicrm/packages
+    git clone "${CACHE_DIR}/eileenmcnaughton/civicrm_developer.git"          -b master               civicrm_developer
 
-   ## http://download.civicrm.org/civicrm-l10n-core/archives/civicrm-l10n-daily.tar.gz => civicrm/l10n
-   ## or https://raw.github.com/civicrm/l10n/master/po/fr_CA/civicrm.mo => civicrm/l10n/fr_CA/LC_MESSAGES/
+    # extract-url --cache-ttl 172800 civicrm=http://download.civicrm.org/civicrm-l10n-core/archives/civicrm-l10n-daily.tar.gz
+    ## or https://raw.github.com/civicrm/l10n/master/po/fr_CA/civicrm.mo => civicrm/l10n/fr_CA/LC_MESSAGES/
   popd
 
 popd
