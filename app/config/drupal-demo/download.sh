64661aa8060aaae477bde824e04a74a003234466
Tim Otten <totten@civicrm.org>
2017-12-15 19:28:15 -0700

    drupal-demo - Replace drush-make with simpler bash script

--- app/config/drupal-demo/download.sh
+++ app/config/drupal-demo/download.sh
@@ -5,21 +5,33 @@
 ###############################################################################
 
 [ -z "$CMS_VERSION" ] && CMS_VERSION=7.x
-[ -z "$VOL_VERSION" ] && VOL_VERSION='4.4-1.x'
-[ -z "$NG_PRFL_VERSION" ] && NG_PRFL_VERSION='v4.6-1.0.1'
+#[ -z "$VOL_VERSION" ] && VOL_VERSION='4.4-1.x'
+[ -z "$VOL_VERSION" ] && VOL_VERSION='master'
+[ -z "$NG_PRFL_VERSION" ] && NG_PRFL_VERSION='v4.6-1.0.2'
 [ -z "$RULES_VERSION" ] && RULES_VERSION='master'
-[ -z "$DISC_VERSION" ] && DISC_VERSION=master
+[ -z "$DISC_VERSION" ] && DISC_VERSION='master'
 
-MAKEFILE="${TMPDIR}/${SITE_TYPE}/${SITE_NAME}/${SITE_ID}.make"
-cvutil_makeparent "$MAKEFILE"
-cat "$SITE_CONFIG_DIR/drush.make.tmpl" \
-  | sed "s;%%CACHE_DIR%%;${CACHE_DIR};" \
-  | sed "s;%%CIVI_VERSION%%;${CIVI_VERSION};" \
-  | sed "s;%%CMS_VERSION%%;${CMS_VERSION};" \
-  | sed "s;%%DISC_VERSION%%;${DISC_VERSION};" \
-  | sed "s;%%RULES_VERSION%%;${RULES_VERSION};" \
-  | sed "s;%%VOL_VERSION%%;${VOL_VERSION};" \
-  | sed "s;%%NG_PRFL_VERSION%%;${NG_PRFL_VERSION};" \
-  > "$MAKEFILE"
+[ -d "$WEB_ROOT.drushtmp" ] && rm -rf "$WEB_ROOT.drushtmp"
+drush8 -y dl drupal-${CMS_VERSION} --destination="$WEB_ROOT.drushtmp" --drupal-project-rename
+mv "$WEB_ROOT.drushtmp/drupal" "$WEB_ROOT"
+[ -d "$WEB_ROOT.drushtmp" ] && rm -rf "$WEB_ROOT.drushtmp"
 
-drush -y make --working-copy "$MAKEFILE" "$WEB_ROOT"
+pushd "$WEB_ROOT"
+  drush8 dl -y libraries-1 redirect-1 webform-4 options_element-1 webform_civicrm-4 views-3 login_destination-1 userprotect-1 devel-1 civicrm_error-2.x-dev
+
+  pushd sites/all/modules
+    git clone "${CACHE_DIR}/civicrm/civicrm-core.git"                        -b "$CIVI_VERSION"      civicrm
+    git clone "${CACHE_DIR}/civicrm/civicrm-drupal.git"                      -b "7.x-$CIVI_VERSION"  civicrm/drupal
+    git clone "${CACHE_DIR}/civicrm/civicrm-packages.git"                    -b "$CIVI_VERSION"      civicrm/packages
+    git clone "${CACHE_DIR}/eileenmcnaughton/civicrm_developer.git"          -b master               civicrm_developer
+    git clone "${CACHE_DIR}/civicrm/civivolunteer.git"                       -b "$VOL_VERSION"       civicrm/tools/extensions/civivolunteer
+    git clone "${CACHE_DIR}/ginkgostreet/org.civicrm.angularprofiles.git"    -b "$NG_PRFL_VERSION"   civicrm/tools/extensions/org.civicrm.angularprofiles
+    git clone "${CACHE_DIR}/civicrm/org.civicoop.civirules.git"              -b "$RULES_VERSION"     civicrm/tools/extensions/org.civicoop.civirules
+    git clone "${CACHE_DIR}/TechToThePeople/civisualize.git"                 -b "master"             civicrm/tools/extensions/civisualize
+    git clone "${CACHE_DIR}/dlobo/org.civicrm.module.cividiscount.git"       -b "$DISC_VERSION"      civicrm/tools/extensions/cividiscount
+
+    extract-url --cache-ttl 172800 civicrm=http://download.civicrm.org/civicrm-l10n-core/archives/civicrm-l10n-daily.tar.gz
+    ## or https://raw.github.com/civicrm/l10n/master/po/fr_CA/civicrm.mo => civicrm/l10n/fr_CA/LC_MESSAGES/
+  popd
+
+popd
