c249853b49e20243301fc251820ae21e488ebfe5
Tim Otten <totten@civicrm.org>
2018-02-04 04:56:15 -0800

    civibuild upgrade-test - Fix path detection
    When calling `civicrm-upgrade-test`, the script assumes that it is executing
    within the `vendor/civicrm/upgrade-test` folder. This assumption helps it
    to locate example datafiles.
    
    However, if you call `civicrm-upgade-test` through buildkit's PATH, this
    doesn't work.  (Why?  `composer` created symlink `bin/civicrm-upgrade-test`
    => `vendor/civicrm/upgrade-test/civicrm-upgrade-test`.)
    
    This is a quick work-around.

--- src/command/upgrade-test.run.sh
+++ src/command/upgrade-test.run.sh
@@ -4,7 +4,7 @@ if grep -q drupal "$WEB_ROOT/index.php" ; then
   cvutil_mkdir "$UPGRADE_LOG_DIR"
   pushd "$PRJDIR/vendor/civicrm/upgrade-test/databases" > /dev/null
     ## Note $ARGS is list of non-option parameters. The first two were action+buildname.
-    civicrm-upgrade-test --db "$CIVI_DB_NAME" --db-args "$CIVI_DB_ARGS" --web "$WEB_ROOT" --out "$UPGRADE_LOG_DIR" --junit-xml "$UPGRADE_LOG_DIR/civicrm-upgrade-test.xml" "${ARGS[@]:2}"
+    ../civicrm-upgrade-test --db "$CIVI_DB_NAME" --db-args "$CIVI_DB_ARGS" --web "$WEB_ROOT" --out "$UPGRADE_LOG_DIR" --junit-xml "$UPGRADE_LOG_DIR/civicrm-upgrade-test.xml" "${ARGS[@]:2}"
   popd > /dev/null
 else
   echo "Skipped. civicrm-upgrade-test currently requires Drupal 7 and Drush."
