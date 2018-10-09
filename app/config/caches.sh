e8f0bc16274fd427c589bbfff52499385eca85cb
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2018-10-09 15:45:33 +0200

    Use civicrm-drupal-8 for Drupal 8 builds instead of civicrm-drupal

--- app/config/caches.sh
+++ app/config/caches.sh
@@ -19,6 +19,7 @@
 git_cache_setup "https://github.com/civicrm/civicrm-backdrop.git"            "$CACHE_DIR/civicrm/civicrm-backdrop.git"
 git_cache_setup "https://github.com/civicrm/civicrm-core.git"                "$CACHE_DIR/civicrm/civicrm-core.git"
 git_cache_setup "https://github.com/civicrm/civicrm-drupal.git"              "$CACHE_DIR/civicrm/civicrm-drupal.git"
+git_cache_setup "https://github.com/civicrm/civicrm-drupal-8.git"            "$CACHE_DIR/civicrm/civicrm-drupal-8.git"
 git_cache_setup "https://github.com/civicrm/civicrm-packages.git"            "$CACHE_DIR/civicrm/civicrm-packages.git"
 git_cache_setup "https://github.com/civicrm/civicrm-joomla.git"              "$CACHE_DIR/civicrm/civicrm-joomla.git"
 git_cache_setup "https://github.com/civicrm/civicrm-wordpress.git"           "$CACHE_DIR/civicrm/civicrm-wordpress.git"
