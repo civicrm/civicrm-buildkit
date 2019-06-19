cde09a5ae1cddc5182dffbc0e03587f2ba759055
Tim Otten <totten@civicrm.org>
2019-06-19 16:28:22 -0700

    wp-empty - For new builds, use `web/` subdir

--- app/config/wp-empty/download.sh
+++ app/config/wp-empty/download.sh
@@ -6,8 +6,8 @@
 [ -z "$CMS_VERSION" ] && CMS_VERSION=latest
 
 echo "[[Download WordPress]]"
-mkdir "$WEB_ROOT"
-pushd "$WEB_ROOT" >> /dev/null
+mkdir "$WEB_ROOT" "$WEB_ROOT/web"
+pushd "$WEB_ROOT/web" >> /dev/null
   "$PRJDIR/bin/wp" core download --version=$CMS_VERSION
   if [ ! -e "wp-cli.yml" ]; then
     cp -a "$SITE_CONFIG_DIR/wp-cli.yml" "wp-cli.yml"
