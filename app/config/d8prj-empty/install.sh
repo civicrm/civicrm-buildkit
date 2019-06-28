318506ba1bf2bdd488a9814f9d40304be3bc51f4
Tim Otten <totten@civicrm.org>
2019-06-27 23:59:21 -0700

    d8prj-empty - Add template for D8 composer project template using `web/../vendor/`

---
+++ app/config/d8prj-empty/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## Transition: Old builds don't have "web/" folder. New builds do.
+## TODO: Simplify sometime after Dec 2019
+## install.sh -- Create config files and databases; fill the databases
+[ -d "$WEB_ROOT/web" ] && CMS_ROOT="$WEB_ROOT/web"
+
+###############################################################################
+## Create virtual-host and databases
+
+amp_install
+
+###############################################################################
+## Setup Drupal (config files, database tables)
+
+drupal8_install
+DRUPAL_SITE_DIR=$(_drupal_multisite_dir "$CMS_URL" "$SITE_ID")
+pushd "${CMS_ROOT}/sites/${DRUPAL_SITE_DIR}" >> /dev/null
+#  drush8 -y updatedb
+#  drush8 -y en libraries
+
+  ## Setup demo user
+  drush8 -y user-create --password="$DEMO_PASS" --mail="$DEMO_EMAIL" "$DEMO_USER"
+popd >> /dev/null
