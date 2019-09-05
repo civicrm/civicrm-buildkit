e50ccaddd1321ade4d88d761b3b07d3af34064e5
Tim Otten <totten@civicrm.org>
2019-09-05 00:18:55 -0700

    drupal-clean - Only get api4 (for dual-version testing) if using master
    This feels wonky and should be removed completely once api4 is migrated into
    core.  But for the moment, it gives a clearer signal for stable/RC
    release-management.

--- app/config/drupal-clean/download.sh
+++ app/config/drupal-clean/download.sh
@@ -19,15 +19,18 @@ pushd "$WEB_ROOT/web"
     git clone "${CACHE_DIR}/civicrm/civicrm-packages.git"                    -b "$CIVI_VERSION"      civicrm/packages
     git clone "${CACHE_DIR}/eileenmcnaughton/civicrm_developer.git"          -b master               civicrm_developer
 
-    ## The core test suite is currently intertwined with APIv4, so we have to get a copy for `Core-PR` and `Core-Matrix` runs.
+    ## The API test suite now supports a dual-version mode (i.e. if APIv3 and APIv4 are both present, it runs some tests against both).
+    ## We want to get some feedback from these tests in master QA, but (at time of writing) the dual-version tests aren't
+    ## fully passing yet (and some pre-requisites haven't been released in api4 stable), so they shouldn't influence planning
+    ## for core's stable/RC versions.
     case "$CIVI_VERSION" in
       master)
         git clone "${CACHE_DIR}/civicrm/api4.git"                            -b "master"             civicrm/ext/api4
         ;;
-      *)
-        EXTCIVIVER=$( php -r '$x=simplexml_load_file("civicrm/xml/version.xml"); echo $x->version_no;' )
-        cv dl -b "@https://civicrm.org/extdir/ver=$EXTCIVIVER|cms=Drupal|status=|ready=/org.civicrm.api4.xml" --to="$WEB_ROOT/web/sites/all/modules/civicrm/ext/api4" --dev
-        ;;
+#      *)
+#        EXTCIVIVER=$( php -r '$x=simplexml_load_file("civicrm/xml/version.xml"); echo $x->version_no;' )
+#        cv dl -b "@https://civicrm.org/extdir/ver=$EXTCIVIVER|cms=Drupal|status=|ready=/org.civicrm.api4.xml" --to="$WEB_ROOT/web/sites/all/modules/civicrm/ext/api4" --dev
+#        ;;
     esac
 
     extract-url --cache-ttl 172800 civicrm=http://download.civicrm.org/civicrm-l10n-core/archives/civicrm-l10n-daily.tar.gz
