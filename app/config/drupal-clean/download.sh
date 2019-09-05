9a5e285cc00d8bbf332f465392315524c7b1d694
Tim Otten <totten@civicrm.org>
2019-09-04 21:50:21 -0700

    drupal-clean - Interim workaround for api4 situation
    Currently, all test builds on core are failing because there's an
    interaction with `api4@master`.  We still want a heads-up when `api4@master`
    is problematic, but also want stable+staging builds to be running code
    that's more representative of what users will get.

--- app/config/drupal-clean/download.sh
+++ app/config/drupal-clean/download.sh
@@ -18,8 +18,17 @@ pushd "$WEB_ROOT/web"
     git clone "${CACHE_DIR}/civicrm/civicrm-drupal.git"                      -b "7.x-$CIVI_VERSION"  civicrm/drupal
     git clone "${CACHE_DIR}/civicrm/civicrm-packages.git"                    -b "$CIVI_VERSION"      civicrm/packages
     git clone "${CACHE_DIR}/eileenmcnaughton/civicrm_developer.git"          -b master               civicrm_developer
-    EXTCIVIVER=$( php -r '$x=simplexml_load_file("civicrm/xml/version.xml"); echo $x->version_no;' )
-    cv dl -b "@https://civicrm.org/extdir/ver=$EXTCIVIVER|cms=Drupal|status=|ready=/org.civicrm.api4.xml" --to="$WEB_ROOT/web/sites/all/modules/civicrm/ext/api4" --dev
+
+    ## The core test suite is currently intertwined with APIv4, so we have to get a copy for `Core-PR` and `Core-Matrix` runs.
+    case "$CIVI_VERSION" in
+      master)
+        git clone "${CACHE_DIR}/civicrm/api4.git"                            -b "master"             civicrm/ext/api4
+        ;;
+      *)
+        EXTCIVIVER=$( php -r '$x=simplexml_load_file("civicrm/xml/version.xml"); echo $x->version_no;' )
+        cv dl -b "@https://civicrm.org/extdir/ver=$EXTCIVIVER|cms=Drupal|status=|ready=/org.civicrm.api4.xml" --to="$WEB_ROOT/web/sites/all/modules/civicrm/ext/api4" --dev
+        ;;
+    esac
 
     extract-url --cache-ttl 172800 civicrm=http://download.civicrm.org/civicrm-l10n-core/archives/civicrm-l10n-daily.tar.gz
     ## or https://raw.github.com/civicrm/l10n/master/po/fr_CA/civicrm.mo => civicrm/l10n/fr_CA/LC_MESSAGES/
