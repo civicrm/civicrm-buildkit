f34924a76d777cb52f939b5749084feb81b7e729
Tim Otten <totten@civicrm.org>
2020-01-03 18:43:19 -0800

    app/config/drupal-empty - Convert from drush-make to match other types
    The main Drupal build types (`drupal-clean`, `drupal-demo`) were switched
    over from drush-make a long time ago, and they're now getting another update
    to use the `drupal_download` helper.
    
    This simply upates `drupal-empty` to match.

--- app/config/drupal-empty/download.sh
+++ app/config/drupal-empty/download.sh
@@ -5,12 +5,9 @@
 ###############################################################################
 
 [ -z "$CMS_VERSION" ] && CMS_VERSION=7.x
-MAKEFILE="${TMPDIR}/${SITE_TYPE}/${SITE_NAME}/${SITE_ID}.make"
-cvutil_makeparent "$MAKEFILE"
-cat "$SITE_CONFIG_DIR/drush.make.tmpl" \
-  | sed "s;%%CACHE_DIR%%;${CACHE_DIR};" \
-  | sed "s;%%CIVI_VERSION%%;${CIVI_VERSION};" \
-  | sed "s;%%CMS_VERSION%%;${CMS_VERSION};" \
-  > "$MAKEFILE"
 
-drush -y make --working-copy "$MAKEFILE" "$WEB_ROOT/web"
+drupal_download
+
+pushd "$WEB_ROOT/web"
+  drush dl -y libraries-1.0 views-3.7 devel
+popd
