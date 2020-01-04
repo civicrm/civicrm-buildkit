3cd112e6ecfa37979eda3bd518b9fe5101b1a814
Tim Otten <totten@civicrm.org>
2020-01-03 19:01:16 -0800

    app/config/backdrop-* - Fix backwards conditional
    In the `download.sh`, the purpose of checking `CMS_VERSION` is to autofill
    it if it's missing.  As previously written, it actually leaves the
    `CMS_VERSION` blank by default - but coerces it to 1.x *only if* someone has
    tried to explicitly set the CMS version.

--- app/config/backdrop-empty/download.sh
+++ app/config/backdrop-empty/download.sh
@@ -6,7 +6,7 @@
 
 git_cache_setup "https://github.com/backdrop/backdrop.git" "$CACHE_DIR/backdrop/backdrop.git"
 
-[ -n "$CMS_VERSION" ] && CMS_VERSION="1.x"
+[ -z "$CMS_VERSION" ] && CMS_VERSION="1.x"
 [ -z "$CIVI_VERSION" ] && CIVI_VERSION=none
 
 backdrop_download
