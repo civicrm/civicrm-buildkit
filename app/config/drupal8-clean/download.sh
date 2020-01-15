82283514ab3297fa0dec0097cea887dbd3e98314
Tim Otten <totten@civicrm.org>
2020-01-14 20:51:41 -0800

    app/config/drupal8-clean - Update to use 'drush dl' + 'composer require" instead of 'drush make'

--- app/config/drupal8-clean/download.sh
+++ app/config/drupal8-clean/download.sh
@@ -4,14 +4,33 @@
 
 ###############################################################################
 
-[ -z "$CMS_VERSION" ] && CMS_VERSION=8.x
+#[ -z "$CMS_VERSION" ] && CMS_VERSION=8.7.x
+[ -z "$CMS_VERSION" ] && CMS_VERSION=8
 
-MAKEFILE="${TMPDIR}/${SITE_TYPE}/${SITE_NAME}/${SITE_ID}.make"
-cvutil_makeparent "$MAKEFILE"
-cat "$SITE_CONFIG_DIR/drush.make.tmpl" \
-  | sed "s;%%CACHE_DIR%%;${CACHE_DIR};" \
-  | sed "s;%%CIVI_VERSION%%;${CIVI_VERSION};" \
-  | sed "s;%%CMS_VERSION%%;${CMS_VERSION};" \
-  > "$MAKEFILE"
+mkdir "$WEB_ROOT"
+drush8 -y dl drupal-${CMS_VERSION} --destination="$WEB_ROOT" --drupal-project-rename
+mv "$WEB_ROOT/drupal" "$WEB_ROOT/web"
 
-drush8 -y make --working-copy "$MAKEFILE" "$WEB_ROOT"
+## Some things that aren't published yet
+mkdir "$WEB_ROOT/repo"
+git clone https://lab.civicrm.org/dev/civicrm-asset-plugin "$WEB_ROOT/repo/civicrm-asset-plugin" -b master-relative
+#git clone https://github.com/civicrm/civicrm-setup/ "$WEB_ROOT/repo/civicrm-setup"
+git clone https://github.com/totten/civicrm-setup/ "$WEB_ROOT/repo/civicrm-setup" -b master-misc
+
+pushd "$WEB_ROOT/web"
+  drush8 dl -y devel-1 libraries userprotect
+  ## TODO /civicrm/civicrm-drupal-8.git /civicrm/civicrm-core.git /civicrm/civicrm-packages.git http://download.civicrm.org/civicrm-l10n-core/archives/civicrm-l10n-daily.tar.gz
+
+  composer config repositories.local path "$WEB_ROOT/repo/*"
+  sed -i.bak 's;"require-dev": \[\],;;' composer.json  ## WTF
+
+  composer require civicrm/civicrm-asset-plugin:'dev-master-relative' civicrm/civicrm-setup:'dev-master-misc as 0.2.99'
+  composer require civicrm/civicrm-{core,packages,drupal-8}:$(civicrm_composer_ver "$CIVI_VERSION") --prefer-source
+
+  ## FIXME: Remove this
+  pushd vendor/civicrm/civicrm-core >> /dev/null
+    patch -p1 < "$SITE_CONFIG_DIR/classloader-packages.patch"
+  popd >> /dev/null
+
+  #composer require roundearth/civicrm-composer-plugin civicrm/civicrm-setup:'dev-master as 0.2.1' civicrm/civicrm-{core,drupal-8}:~${CIVI_VERSION}.0 pear/pear_exception:'1.0.1 as 1.0.0'
+popd
