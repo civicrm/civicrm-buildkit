3d58140b6ffc027c7c15bea01a69e0b461d1ed85
Tim Otten <totten@civicrm.org>
2020-01-29 14:30:26 -0800

    {d8prj,drupal8}-clean - Tighten 'setup' override. Prettify CIVI_VERSION ref.

--- app/config/d8prj-clean/download.sh
+++ app/config/d8prj-clean/download.sh
@@ -6,12 +6,13 @@
 
 #[ -z "$CMS_VERSION" ] && CMS_VERSION=8.7.x
 [ -z "$CMS_VERSION" ] && CMS_VERSION=8.x-dev
+CIVI_VERSION_COMP=$(civicrm_composer_ver "$CIVI_VERSION")
 
 composer create-project drupal-composer/drupal-project:"$CMS_VERSION" "$WEB_ROOT" --no-interaction
 
 pushd "$WEB_ROOT" >> /dev/null
   composer require drupal/{devel,libraries,userprotect}
-  composer require civicrm/civicrm-asset-plugin:'~1.0.0' civicrm/civicrm-setup:'dev-master as 0.2.99' civicrm/civicrm-{core,packages,drupal-8}:$(civicrm_composer_ver "$CIVI_VERSION") pear/pear_exception:'1.0.1 as 1.0.0' --prefer-source
+  composer require civicrm/civicrm-asset-plugin:'~1.0.0' civicrm/civicrm-setup:'0.4.0 as 0.2.99' civicrm/civicrm-{core,packages,drupal-8}:"$CIVI_VERSION_COMP" pear/pear_exception:'1.0.1 as 1.0.0' --prefer-source
 
   ## FIXME: All of the following should be removed/replaced as things get cleaner.
   composer require "cache/integration-tests:dev-master#b97328797ab199f0ac933e39842a86ab732f21f9" ## Issue: it's a require-dev in civicrm-core for E2E/Cache/*; how do we pull in civi require-dev without all other require-dev?
--- app/config/drupal8-clean/download.sh
+++ app/config/drupal8-clean/download.sh
@@ -6,6 +6,7 @@
 
 #[ -z "$CMS_VERSION" ] && CMS_VERSION=8.7.x
 [ -z "$CMS_VERSION" ] && CMS_VERSION=8
+CIVI_VERSION_COMP=$(civicrm_composer_ver "$CIVI_VERSION")
 
 mkdir "$WEB_ROOT"
 drush8 -y dl drupal-${CMS_VERSION} --destination="$WEB_ROOT" --drupal-project-rename
@@ -13,7 +14,7 @@ mv "$WEB_ROOT/drupal" "$WEB_ROOT/web"
 
 pushd "$WEB_ROOT/web" >> /dev/null
   drush8 dl -y devel-1 libraries userprotect
-  composer require civicrm/civicrm-asset-plugin:'~1.0.0' civicrm/civicrm-setup:'dev-master as 0.2.99' civicrm/civicrm-{core,packages,drupal-8}:$(civicrm_composer_ver "$CIVI_VERSION") --prefer-source
+  composer require civicrm/civicrm-asset-plugin:'~1.0.0' civicrm/civicrm-setup:'0.4.0 as 0.2.99' civicrm/civicrm-{core,packages,drupal-8}:"$CIVI_VERSION_COMP" --prefer-source
 
   ## FIXME: All of the following should be removed/replaced as things get cleaner.
   composer require "cache/integration-tests:dev-master#b97328797ab199f0ac933e39842a86ab732f21f9" ## Issue: it's a require-dev in civicrm-core for E2E/Cache/*; how do we pull in civi require-dev without all other require-dev?
