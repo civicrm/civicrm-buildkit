0cc32512b5271f1766c1f893a7fa65d224123662
Tim Otten <totten@civicrm.org>
2020-06-17 15:40:04 -0700

    bin/update-ci-buildkit.sh - Update to work without old launcher

--- nix/bin/update-ci-buildkit.sh
+++ nix/bin/update-ci-buildkit.sh
@@ -3,7 +3,7 @@ for OWNER in jenkins publisher ; do
   for PROF in dfl min max old edge ; do
     if [ -d "/home/$OWNER/bknix-$PROF" ]; then
       echo "Update \"$PROF\" for user \"$OWNER\""
-      su - $OWNER -c 'eval $(use-bknix '$PROF') && bknix update'
+      su - $OWNER -c 'eval $(use-bknix '$PROF') && cd $LOCO_PRJ && git pull && ./bin/civi-download-tools'
     fi
   done
 done
