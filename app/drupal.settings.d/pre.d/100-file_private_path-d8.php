fd053c6008ea7575015c23e2b650b948cce13981
eileen <emcnaughton@wikimedia.org>
2020-08-14 11:33:34 +1200

    Enotice fix on d7
    Seeing an enotice fix on d7 on code clearly intended for D8
    
    Change-Id: I6085a30c9340d3d38271a0891b40ef700a3fe6e3

--- app/drupal.settings.d/pre.d/100-file_private_path-d8.php
+++ app/drupal.settings.d/pre.d/100-file_private_path-d8.php
@@ -2,6 +2,6 @@
 
 global $civibuild, $settings;
 // Example CMS_VERSIONs: '7', '7.23', '7.x', '8', '8.8', '8.x'. Note that the '8.x' case would mess up `version_compare()`.
-if (empty($settings['file_private_path']) && isset($civibuild['CMS_VERSION']) && $civibuild['CMS_VERSION'][0] === '8') {
+if (isset($civibuild['CMS_VERSION'][0]) && empty($settings['file_private_path']) && $civibuild['CMS_VERSION'][0] === '8') {
   $settings['file_private_path'] = $civibuild['PRIVATE_ROOT'] . DIRECTORY_SEPARATOR . $civibuild['DRUPAL_SITE_DIR'];
 }
