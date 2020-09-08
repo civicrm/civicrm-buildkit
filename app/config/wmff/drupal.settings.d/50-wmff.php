1eb85d800fa466936783e7e5c716ad83a6856449
eileen <emcnaughton@wikimedia.org>
2020-09-08 13:47:49 +1200

    WMF-specific Define db prefixes for fredge db tables.
    Change-Id: Ie0731816987ee9ff07926070f48d33597d8942bf

--- app/config/wmff/drupal.settings.d/50-wmff.php
+++ app/config/wmff/drupal.settings.d/50-wmff.php
@@ -4,5 +4,11 @@ global $databases;
 
 $databases['donations']['default'] = $databases['default']['default'];
 $databases['fredge']['default'] = $databases['default']['default'];
+$databases['default']['default']['prefix'] = [
+  'default' => '',
+  'payments_initial' => '`fredge`.',
+  'payments_fraud' => '`fredge`.',
+  'payments_fraud_breakdown' => '`fredge`.',
+];
 
-define( 'WMF_SOURCE_REVISION', 'integration' );
+define('WMF_SOURCE_REVISION', 'integration');
