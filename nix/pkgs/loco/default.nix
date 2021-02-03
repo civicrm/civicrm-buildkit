75dadf717c7772b4522fd7b184f8099f89c5b61e
Tim Otten <totten@civicrm.org>
2021-02-02 22:59:14 -0800

    Update to loco v0.4.4. More robust w/PHP on macOS.

--- nix/pkgs/loco/default.nix
+++ nix/pkgs/loco/default.nix
@@ -7,8 +7,8 @@ in stdenv.mkDerivation rec {
 
     name = "loco";
     src = pkgs.fetchurl {
-      url = https://github.com/totten/loco/releases/download/v0.4.3/loco-0.4.3.phar;
-      sha256 = "0g7avj2g0v5nbmnn2dzpdi8m0zw7igrb93gqqq81sir1kh1s7qm2";
+      url = https://github.com/totten/loco/releases/download/v0.4.4/loco-0.4.4.phar;
+      sha256 = "0bg7q0h835kvp9zhvk7a5ymfxazm3c16xh3x0yc7z0m2cx2s7n10";
       executable = true;
     };
     buildInputs = [ ];
