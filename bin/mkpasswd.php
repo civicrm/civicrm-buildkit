4d1c833f9b475c22121f56c9e4880fc4d35fbfcd
Seamus Lee <seamuslee001@gmail.com>
2021-05-19 14:14:35 +1000

    Update mkpassword for php7.4/php8 compatability

--- bin/mkpasswd.php
+++ bin/mkpasswd.php
@@ -4,7 +4,7 @@ function createRandom($len, $alphabet) {
   $alphabetSize = strlen($alphabet);
   $result = '';
   for ($i = 0; $i < $len; $i++) {
-    $result .= $alphabet{rand(1, $alphabetSize) - 1};
+    $result .= $alphabet[rand(1, $alphabetSize) - 1];
   }
   return $result;
 }
