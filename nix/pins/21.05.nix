897f4eccbf0f0164eb622a9466dc87cd1a80a11f
Tim Otten <totten@civicrm.org>
2021-12-03 20:48:23 +0000

    pins/21.05.nix - Bump to newer iteration

--- nix/pins/21.05.nix
+++ nix/pins/21.05.nix
@@ -1,7 +1,7 @@
 /**
- * v21.05, with official backports circa Sep 24 2021
+ * v21.05, with official backports circa Dec 3 2021
  */
 fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/df409303ab906216e7fb04fd0cbfd73f03aff1f7.tar.gz";
-  sha256 = "09cr2lxnp8m406nb4ka1kja62cl5cxzh9dl1g57s2k86313sw37p";
+  url = "https://github.com/nixos/nixpkgs/archive/7e9b0dff974c89e070da1ad85713ff3c20b0ca97.tar.gz";
+  sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
 }
