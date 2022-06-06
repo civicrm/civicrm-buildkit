49cf55f34d0fe538acce87076093d48afb643366
Tim Otten <totten@civicrm.org>
2022-06-06 14:33:59 -0700

    D8+ - Fix configuration of file_private_path
    1. Enable on D9/D10
    2. Ignore composer version-constraint chars

--- app/drupal.settings.d/pre.d/100-file_private_path-d8.php
+++ app/drupal.settings.d/pre.d/100-file_private_path-d8.php
@@ -1,7 +1,8 @@
 <?php
 
 global $civibuild, $settings;
-// Example CMS_VERSIONs: '7', '7.23', '7.x', '8', '8.8', '8.x'. Note that the '8.x' case would mess up `version_compare()`.
-if (isset($civibuild['CMS_VERSION'][0]) && empty($settings['file_private_path']) && $civibuild['CMS_VERSION'][0] === '8') {
+// Example CMS_VERSIONs: '7', '7.23', '7.x', '8', '8.3.4', '8.x', '^9'. Note that the '8.x' and `^9' case would mess up `version_compare()`.
+$cmsMajorVer = ltrim(explode('.', $civibuild['CMS_VERSION'] ?? '')[0], '^~><=');
+if ($cmsMajorVer && empty($settings['file_private_path']) && $cmsMajorVer >= 8) {
   $settings['file_private_path'] = $civibuild['PRIVATE_ROOT'] . DIRECTORY_SEPARATOR . $civibuild['DRUPAL_SITE_DIR'];
 }
