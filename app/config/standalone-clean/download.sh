3efece1331dfd1947502e7d93cd8610477758f25
Mathieu Lutfy <mathieu@civicrm.org>
2022-07-07 10:53:00 -0400

    standalone: update git repo URL

--- app/config/standalone-clean/download.sh
+++ app/config/standalone-clean/download.sh
@@ -6,7 +6,7 @@
 
 # @todo Requires that we register with packagist?
 # mkdir "$WEB_ROOT"
-# composer create-project mlutfy/civicrm-standalone "$WEB_ROOT" --no-interaction
-git clone https://github.com/mlutfy/civicrm-standalone $WEB_ROOT
+# composer create-project civicrm/civicrm-standalone "$WEB_ROOT" --no-interaction
+git clone https://github.com/civicrm/civicrm-standalone $WEB_ROOT
 cd $WEB_ROOT
 composer install
