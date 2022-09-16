255a4d851af303106ab6028588f5866d9a430dd2
Tim Otten <totten@civicrm.org>
2022-09-15 22:05:58 -0700

    nix/pins - Update v22.05

--- nix/pins/22.05-pre.nix
+++
@@ -1,7 +0,0 @@
-/**
- * v22.05 pre  with official backports circa Feb 5 2021
- */
-fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/6794a2c3f67a92f374e02c52edf6442b21a52ecb.tar.gz";
-  sha256 = "05dyh9lbqijbafzzdjvha6jczcji753vrr5nkz34bbcn61anm4cw";
-}
