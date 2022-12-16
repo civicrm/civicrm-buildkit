302ea7663537d1725e3e54e8bbbd647f4b3644cf
Tim Otten <totten@civicrm.org>
2022-12-16 15:56:34 -0800

    update-ci-buildkit.sh - Be a little more forgiving

--- nix/bin/update-ci-buildkit.sh
+++ nix/bin/update-ci-buildkit.sh
@@ -16,7 +16,7 @@ for OWNER in jenkins publisher ; do
   for PROF in dfl min max old edge ; do
     if [ -d "/home/$OWNER/bknix-$PROF" ]; then
       echo "Update \"$PROF\" for user \"$OWNER\""
-      su - $OWNER -c 'eval $(use-bknix '$PROF') && cd $LOCO_PRJ && git pull && ./bin/civi-download-tools'
+      su - $OWNER -c 'eval $(use-bknix '$PROF') && cd $LOCO_PRJ && git checkout -- package-lock.json && git pull && ./bin/civi-download-tools'
     fi
   done
 done
