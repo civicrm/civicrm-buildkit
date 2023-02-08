b45de918af1c601fdb287db54b11c5b28e7f6d8b
Tim Otten <totten@civicrm.org>
2023-02-08 02:10:03 -0800

    loco - Update to 0.7.1

--- nix/pkgs/loco/default.nix
+++ nix/pkgs/loco/default.nix
@@ -7,8 +7,8 @@ in stdenv.mkDerivation rec {
 
     name = "loco";
     src = pkgs.fetchurl {
-      url = https://github.com/totten/loco/releases/download/v0.7.0/loco-0.7.0.phar;
-      sha256 = "6lgw9iGNmkdlApHcb0AD3wgcMwP3RxD+Po84NCYbdsA=";
+      url = https://github.com/totten/loco/releases/download/v0.7.1/loco-0.7.1.phar;
+      sha256 = "ckF3GN1rQYLZIrXG5mdS/kfRl3il3/PpH6OwCqVagsc=";
       executable = true;
     };
     buildInputs = [ ];
