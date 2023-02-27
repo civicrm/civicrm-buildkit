adbbca139a0947c57f12a80edcd4cff46464a732
Tim Otten <totten@civicrm.org>
2023-02-26 22:10:32 -0800

    update-ci-buildkit.sh - Add support for runner-style hosts

--- nix/bin/update-ci-buildkit.sh
+++ nix/bin/update-ci-buildkit.sh
@@ -13,9 +13,16 @@ source "$BINDIR/../lib/common.sh"
 assert_root_user
 
 for OWNER in jenkins publisher ; do
+  if [ -e "/etc/bknix-ci/worker-n" ]; then
+    if [ -d "/home/$OWNER/bknix" ]; then
+      echo "Update \"bknix\" for user \"$OWNER\""
+      su - $OWNER -c 'eval $(use-bknix min -N0) && cd $LOCO_PRJ && git checkout -- package-lock.json && git pull && ./bin/civi-download-tools'
+    fi
+  fi
+
   for PROF in dfl min max old edge ; do
     if [ -d "/home/$OWNER/bknix-$PROF" ]; then
-      echo "Update \"$PROF\" for user \"$OWNER\""
+      echo "Update \"bknix-$PROF\" for user \"$OWNER\""
       su - $OWNER -c 'eval $(use-bknix '$PROF') && cd $LOCO_PRJ && git checkout -- package-lock.json && git pull && ./bin/civi-download-tools'
     fi
   done
