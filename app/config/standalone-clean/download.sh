0eac6ae8d844a414faead764ceaabbc3bc70089c
Tim Otten <totten@civicrm.org>
2023-03-09 02:42:51 -0800

    standalone - Set permissions. Publish assets.

--- app/config/standalone-clean/download.sh
+++ app/config/standalone-clean/download.sh
@@ -7,6 +7,11 @@
 # @todo Requires that we register with packagist?
 # mkdir "$WEB_ROOT"
 # composer create-project civicrm/civicrm-standalone "$WEB_ROOT" --no-interaction
-git clone https://github.com/civicrm/civicrm-standalone $WEB_ROOT
-cd $WEB_ROOT
-composer install
+git clone https://github.com/civicrm/civicrm-standalone "$WEB_ROOT"
+# git clone https://github.com/artfulrobot/civicrm-standalone -b artfulrobot-clean-install-paths "$WEB_ROOT"
+
+pushd "$WEB_ROOT"
+  amp datadir "./data" "./web/uploads"
+  composer install
+  composer civicrm:publish
+popd
