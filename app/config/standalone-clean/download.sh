ef748f6531ab9a5276ad68a8539dfd4ea54256c4
Tim Otten <totten@civicrm.org>
2023-03-09 14:01:36 -0800

    standalone - Respect "--cms-ver". Allow references to forks/branches.

--- app/config/standalone-clean/download.sh
+++ app/config/standalone-clean/download.sh
@@ -4,13 +4,20 @@
 
 ###############################################################################
 
-# @todo Requires that we register with packagist?
-# mkdir "$WEB_ROOT"
-# composer create-project civicrm/civicrm-standalone "$WEB_ROOT" --no-interaction
+[ -z "$CMS_VERSION" ] && CMS_VERSION=master
+## Interpreted as tag/branch of "civicrm-standalone.git".
+## May use git remotes to referencing Github ("{user}/{branch}").
+
 git clone https://github.com/civicrm/civicrm-standalone "$WEB_ROOT"
-# git clone https://github.com/artfulrobot/civicrm-standalone -b artfulrobot-clean-install-paths "$WEB_ROOT"
 
 pushd "$WEB_ROOT"
+  if [[ "$CMS_VERSION" == *"/"* ]]; then
+    _git_owner=$(dirname "$CMS_VERSION")
+    git remote add "$_git_owner" "https://github.com/${_git_owner}/civicrm-standalone.git"
+    git fetch "$_git_owner"
+  fi
+  git checkout "$CMS_VERSION"
+
   amp datadir "./data" "./web/uploads"
   composer install
   composer civicrm:publish
