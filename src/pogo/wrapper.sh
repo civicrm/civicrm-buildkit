3acbcb4aba4ca0c9eb4dbb3e4da3b86afa7a0673
Tim Otten <totten@civicrm.org>
2023-03-23 16:51:56 -0700

    civi-test-{pr,job} - Don't override TMPDIR
    Same idea as c7e7a70de1f75a7751ffe90f511a7eeac5748a32; different scripts.

--- src/pogo/wrapper.sh
+++ src/pogo/wrapper.sh
@@ -15,7 +15,5 @@ function absdirname() {
 SELF=$(basename "$0")
 BINDIR=$(absdirname "$0")
 PRJDIR=$(dirname "$BINDIR")
-[ -z "$CIVIBUILD_HOME" ] && TMPDIR="$PRJDIR/app/tmp" || TMPDIR="$CIVIBUILD_HOME/.civibuild/tmp"
 
-#exec pogo --run-mode=local --dl="$TMPDIR/$SELF-{PHP_XY}" -- "$PRJDIR/src/pogo/$SELF.php" "$@"
 exec pogo --run-mode=local --dl="$PRJDIR/extern/$SELF-php{PHP_XY}" -- "$PRJDIR/src/pogo/$SELF.php" "$@"
