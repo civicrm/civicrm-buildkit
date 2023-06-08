7342083ce8982574a90ab2700c80263f55de9215
Tim Otten <totten@civicrm.org>
2023-06-07 19:43:28 -0700

    Add app/config/drupal10-clean

---
+++ app/config/drupal10-clean/download.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## download.sh -- Download Drupal and CiviCRM
+
+###############################################################################
+
+#[ -z "$CMS_VERSION" ] && CMS_VERSION=8.7.x
+[ -z "$CMS_VERSION" ] && CMS_VERSION='^10'
+
+mkdir "$WEB_ROOT"
+composer create-project drupal/recommended-project:"$CMS_VERSION" "$WEB_ROOT" --no-interaction
+
+pushd "$WEB_ROOT" >> /dev/null
+  composer_allow_common_plugins
+  composer require drupal/userprotect
+  composer require drupal/devel
+  composer require drush/drush
+  ## Some D8 builds include a specific revision of phpunit, but Civi uses standalone phpunit (PHAR)
+  if composer info | grep -q ^phpunit/phpunit\ ; then
+    composer config "discard-changes" true ## Weird. phpcs has changes which interfere with other work.
+    composer remove --dev drupal/core-dev
+    composer remove phpunit/phpunit
+    composer install --no-dev --no-interaction
+  fi
+  drupal8_po_download "${CIVICRM_LOCALES:-de_DE}" "drupal-$( _drupalx_version x.y ).x" devel-5.1.x
+  civicrm_download_composer_d8
+popd >> /dev/null
