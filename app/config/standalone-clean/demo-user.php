4df232200722f33c4614e2e974c7fc76b1006930
Tim Otten <totten@civicrm.org>
2023-06-27 00:58:02 -0700

    standalone-clean - Create demo user

---
+++ app/config/standalone-clean/demo-user.php
@@ -0,0 +1,39 @@
+<?php
+
+// Add a demo user.
+
+if (PHP_SAPI !== 'cli') {
+  die("phpunit can only be run from command line.");
+}
+
+foreach (['DEMO_EMAIL', 'DEMO_USER', 'DEMO_PASS'] as $var) {
+  if (empty(getenv($var))) {
+    throw new \Exception("Missing: $var");
+  }
+}
+
+// Since users+contacts are stored in same DB, we can do transaction.
+CRM_Core_Transaction::create()->run(function () {
+
+  $contactID = \Civi\Api4\Contact::create(FALSE)
+    ->setValues([
+      'contact_type' => 'Individual',
+      'first_name' => 'Demo',
+      'last_name' => 'User',
+    ])
+    ->execute()->first()['id'];
+  $adminEmail = getenv('DEMO_EMAIL');
+  $params = [
+    'cms_name' => getenv('DEMO_USER'),
+    'cms_pass' => getenv('DEMO_PASS'),
+    'notify' => FALSE,
+    $adminEmail => $adminEmail,
+    'contactID' => $contactID,
+  ];
+  $userID = \CRM_Core_BAO_CMSUser::create($params, $adminEmail);
+
+  \Civi\Api4\UserRole::create(FALSE)
+    ->setValues(['role_id.name' => 'Administrator', 'user_id' => $userID])
+    ->execute();
+
+});
