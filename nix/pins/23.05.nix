d29d095c62b29c79096132b7499940e33380f1ce
Tim Otten <totten@civicrm.org>
2023-08-03 20:06:45 -0700

    nix - Add nixpkgs v23.05

---
+++ nix/pins/23.05.nix
@@ -0,0 +1,7 @@
+/**
+ * v23.05
+ */
+fetchTarball {
+  url = "https://github.com/nixos/nixpkgs/archive/4ecab3273592f27479a583fb6d975d4aba3486fe.tar.gz";
+  sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
+}
