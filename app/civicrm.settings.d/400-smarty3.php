0f9b72a3165707088f8364e488f53c92c66bd0de
Tim Otten <totten@civicrm.org>
2023-11-08 23:47:51 -0800

    400-smarty3.php - Fix path-computation for standard D8/9/10 layouts
    * Substantively: the folder name needs a `-` not `_`
    * Stylistically: a little easier to read with `dirname()`

--- app/civicrm.settings.d/400-smarty3.php
+++ app/civicrm.settings.d/400-smarty3.php
@@ -17,11 +17,12 @@ else {
       break;
 
     case 'Drupal8':
-      $packages_path = $GLOBALS['_CV']['CIVI_CORE'] . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'civicrm_packages';
+      $packages_path = dirname($GLOBALS['_CV']['CIVI_CORE']) . DIRECTORY_SEPARATOR . 'civicrm-packages';
       break;
 
   }
 }
+
 if (getenv('SMARTY3_ENABLE') && file_exists($packages_path . DIRECTORY_SEPARATOR . 'smarty3' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php') && !defined('CIVICRM_SMARTY3_AUTOLOAD_PATH')) {
   define('CIVICRM_SMARTY3_AUTOLOAD_PATH', $packages_path . DIRECTORY_SEPARATOR . 'smarty3' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
 }
