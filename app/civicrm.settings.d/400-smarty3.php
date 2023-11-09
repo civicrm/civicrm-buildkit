8d8e7aa326f6905ea8e4f990296ef52c923a51a9
Seamus Lee <seamuslee001@gmail.com>
2023-11-09 13:27:53 +1100

    Handle for when CV is not in globals

--- app/civicrm.settings.d/400-smarty3.php
+++ app/civicrm.settings.d/400-smarty3.php
@@ -1,18 +1,26 @@
 <?php
 
-switch($GLOBALS['_CV']['CIVI_UF']) {
-  case 'Drupal':
-  case 'Backdrop':
-  case 'WordPress':
-  case 'Standalone':
-  case 'Joomla':
-    $packages_path = $GLOBALS['_CV']['CIVI_CORE'] . DIRECTORY_SEPARATOR . 'packages';
-    break;
+if (!isset($GLOBALS['_CV']) && isset($GLOBALS['civicrm_paths']) && !empty($GLOBALS['civicrm_paths']['civicrm.packages'])) {
+  $packages_path = $GLOBALS['civicrm_paths']['civicrm.packages']['path'];
+}
+elseif (!isset($GLOBALS['_CV'])) {
+  return;
+}
+else {
+  switch($GLOBALS['_CV']['CIVI_UF']) {
+   case 'Drupal':
+     case 'Backdrop':
+    case 'WordPress':
+    case 'Standalone':
+    case 'Joomla':
+      $packages_path = $GLOBALS['_CV']['CIVI_CORE'] . DIRECTORY_SEPARATOR . 'packages';
+      break;
 
-  case 'Drupal8':
-    $packages_path = $GLOBALS['_CV']['CIVI_CORE'] . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'civicrm_packages';
-    break;
+    case 'Drupal8':
+      $packages_path = $GLOBALS['_CV']['CIVI_CORE'] . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'civicrm_packages';
+      break;
 
+  }
 }
 if (getenv('SMARTY3_ENABLE') && file_exists($packages_path . DIRECTORY_SEPARATOR . 'smarty3' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php') && !defined('CIVICRM_SMARTY3_AUTOLOAD_PATH')) {
   define('CIVICRM_SMARTY3_AUTOLOAD_PATH', $packages_path . DIRECTORY_SEPARATOR . 'smarty3' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
