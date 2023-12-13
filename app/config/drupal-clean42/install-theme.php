8990261493bc629f5400fb1d082eb8beed724c3d
Tim Otten <totten@civicrm.org>
2023-12-13 06:51:16 -0800

    Remove various stale artifacts (drupal-clean42, etc)

--- app/config/drupal-clean42/install-theme.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// Enable blocks
-db_query("update block set region='sidebar_first' where theme='bartik' and module='user' and delta='login'");
-db_query("update block set region='sidebar_first' where theme='bartik' and module='system' and delta='navigation'");
