674d537bf9e30a2a6c784250edc744ea0f165047
Tim Otten <totten@civicrm.org>
2023-12-13 08:23:07 -0800

    Use git_cache_clone_id (dist, standalone-dev)

--- app/config/standalone-dev/download.sh
+++ app/config/standalone-dev/download.sh
@@ -12,8 +12,8 @@ mkdir -p "$WEB_ROOT" "$WEB_ROOT/web" "$WEB_ROOT/web/uploads" "$WEB_ROOT/data"
 pushd "$WEB_ROOT"
   amp datadir "./data" "./web/uploads"
 
-  git clone ${CACHE_DIR}/civicrm/civicrm-core.git                     -b "$CIVI_VERSION" web/core
-  git clone ${CACHE_DIR}/civicrm/civicrm-packages.git                 -b "$CIVI_VERSION" web/core/packages
+  git_cache_clone_id civicrm/civicrm-core                             -b "$CIVI_VERSION" web/core
+  git_cache_clone_id civicrm/civicrm-packages                         -b "$CIVI_VERSION" web/core/packages
 popd
 
 pushd "$WEB_ROOT/web/core"
