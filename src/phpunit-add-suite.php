41c362a3c0e8129ddf955c931b887012dfa5a95d
Tim Otten <totten@civicrm.org>
2024-12-16 23:45:48 -0800

    phpunit-add-suite.php - Allow mix of directories and/or plain files

--- src/phpunit-add-suite.php
+++ src/phpunit-add-suite.php
@@ -5,13 +5,15 @@
  */
 
 if ($argc < 3) {
-  fwrite(STDERR, "Usage: php add-suite.php <suite-name> <directory1> [<directory2> ...] < phpunit.xml.dist > phpunit.xml\n");
+  fwrite(STDERR, "Usage: php add-suite.php SUITE-NAME FILES... < phpunit.xml.dist > phpunit.xml\n");
+  fwrite(STDERR, "  SUITE-NAME:  Logical name of the test-suite (Ex 'phpunit-e2e')\n");
+  fwrite(STDERR, "  FILES...:    List of test-files or test-folders to include in the suite\n");
   exit(1);
 }
 
 // Get the suite name and directories from command-line arguments
 $suiteName = $argv[1];
-$directories = array_slice($argv, 2);
+$files = array_slice($argv, 2);
 
 // Load the XML template from STDIN
 $xml = new DOMDocument();
@@ -33,10 +35,17 @@ $testsuite = $xml->createElement('testsuite');
 $testsuite->setAttribute('name', $suiteName);
 
 // Add each directory as a <directory> element within the new <testsuite>
-foreach ($directories as $directory) {
-  if (is_dir($directory)) {
-    $dirElement = $xml->createElement('directory', $directory);
+foreach ($files as $file) {
+  if (is_dir($file)) {
+    $dirElement = $xml->createElement('directory', $file);
     $testsuite->appendChild($dirElement);
+  }
+  elseif (preg_match(';Test\.php$;', $file)) {
+    $fileElement = $xml->createElement('file', $file);
+    $testsuite->appendChild($fileElement);
+  }
+  else {
+    fprintf(STDERR, "Warning: Ignore requested file %s\n", $file);
   }
 }
 
