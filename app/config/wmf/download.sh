920b687c92c4554a2252a0b8eb8a3bc4fa4f6328
Elliott Eggleston <ejegg@ejegg.com>
2025-05-23 19:57:17 -0400

    Add new WMF config for standalone CiviCRM

---
+++ app/config/wmf/download.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set +x
+git_cache_setup "https://gerrit.wikimedia.org/r/wikimedia/fundraising/crm.git" "$CACHE_DIR/wikimedia/fundraising/crm.git"
+set -x
+
+git clone --recursive "$CACHE_DIR/wikimedia/fundraising/crm.git" "$WEB_ROOT"
+
+pushd "$WEB_ROOT"
+  ## Reset the .git/config to match remotes in .gitmodules
+  git submodule sync
+
+  ## Checkout the proper revisions
+  git submodule update --init --recursive
+
+popd
