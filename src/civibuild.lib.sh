13dee34bc88c5649aa399c99d3950ba4c325a960
eileen <emcnaughton@wikimedia.org>
2025-05-25 21:19:51 -0700

    address deprecation warning
    ! [NOTE] Option --cms-base-url is a deprecated alias for --url (-l)

--- src/civibuild.lib.sh
+++ src/civibuild.lib.sh
@@ -758,7 +758,7 @@ function civicrm_install_cv() {
     installOpts+=("-m" "settings.extensionsDir=$CIVI_EXT_DIR")
     installOpts+=("-m" "settings.extensionsURL=$CIVI_EXT_URL")
   fi
-  cv core:install -vv -f --cms-base-url="$CMS_URL" --db="$CIVI_DB_DSN" -m "siteKey=$CIVI_SITE_KEY" "${installOpts[@]}"
+  cv core:install -vv -f --url="$CMS_URL" --db="$CIVI_DB_DSN" -m "siteKey=$CIVI_SITE_KEY" "${installOpts[@]}"
   local settings=$( cv ev 'echo CIVICRM_SETTINGS_PATH;' )
   cvutil_inject_settings "$settings" "civicrm.settings.d"
   civicrm_update_domain
