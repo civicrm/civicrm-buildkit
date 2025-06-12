b1f02d1f20d93625c3aaccd845b37021aed44b28
Tim Otten <totten@civicrm.org>
2025-06-12 16:31:14 -0700

    tools/*/run.php - Drop boilerplate references to POGO_AUTOLOAD

--- tools/find-stale-builds/run.php
+++ tools/find-stale-builds/run.php
@@ -1,9 +1,5 @@
 #!/usr/bin/env php
 <?php
 
-
-$_SERVER["POGO_AUTOLOAD"] = $_ENV["POGO_AUTOLOAD"] =  __DIR__ . "/vendor/autoload.php";
-putenv("POGO_AUTOLOAD=" . $_ENV["POGO_AUTOLOAD"]);
-
 require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . "/script.php";
