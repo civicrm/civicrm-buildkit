ff66f57a89f37212522d752dcab126b8ed3c6254
Tim Otten <totten@civicrm.org>
2025-06-19 13:44:47 -0700

    releaser - Move computation of RELEASE_TMPDIR from `bin/releaser` to `tools/releaser/run.php`

--- tools/releaser/run.php
+++ tools/releaser/run.php
@@ -1,5 +1,13 @@
 #!/usr/bin/env php
 <?php
 
+$prjDir = dirname(__DIR__, 2);
+$tmpDir = empty(getenv('CIVIBUILD_HOME')) ? ("$prjDir/app/tmp") : (getenv('CIVIBUILD_HOME') . '/.civibuild/tmp');
+$_ENV['RELEASE_TMPDIR'] = "$tmpDir/releaser";
+putenv("RELEASE_TMPDIR=" . $_ENV['RELEASE_TMPDIR']);
+if (!is_dir($_ENV['RELEASE_TMPDIR'])) {
+  mkdir($_ENV['RELEASE_TMPDIR'], 0777, TRUE);
+}
+
 require_once __DIR__ . "/vendor/autoload.php";
 require_once file_exists(__DIR__ . "/script.php") ? __DIR__ . "/script.php" : $_ENV["POGO_SCRIPT"];
