fcfbb809177d6c11e9187a38a6923e9e4697f817
Tim Otten <totten@civicrm.org>
2025-06-24 01:18:01 -0700

    Add more mysql, mariadb's to nix/scripts/try-profiles.sh

--- nix/scripts/try-profiles.sh
+++ nix/scripts/try-profiles.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env ebash
+#!/usr/bin/env bash
 
 #for PHP in php73 php84 ; do
 #  for DB in m57 ; do
 for PHP in php73 php74 php80 php81 php82 php83 php84 ; do
-  for DB in m57 m84 m90 r106 ; do
+  for DB in m57 m84 m90 m93 r106 r1011 ; do
     BKPROF="${PHP}${DB}"
     echo -n "$BKPROF: "
     nix-shell -A "$BKPROF" --run 'echo $( php --version | head -n1 ) $(mysql --version | head -n1)'
