0a91f0ee798683c2d94beea76a709d712afd3587
Tim Otten <totten@civicrm.org>
2025-06-25 15:48:53 -0700

    nix - Update PHP versions to split between new/old ICU versions

--- nix/pins/25.05.nix
+++ nix/pins/25.05.nix
@@ -2,9 +2,14 @@
  * v25.05
  */
 fetchTarball {
+
+  ## Unofficial backports circa June 25, 2025; totten/pkgs2505-addenda; update icuXX
+  url = "https://github.com/nixos/nixpkgs/archive/7444fba07bd1988c5203bf198c3241afcf1a16e1.tar.gz";
+  sha256 = "19ypdda8vlc230lqjr9q7gs633mdry8i58mi864zdiqi66v3nwhh";
+
   ## Unofficial backports circa June 24, 2025; totten/pkgs2505-addenda; mysql93
-  url = "https://github.com/nixos/nixpkgs/archive/dad9999266b41da929ca8b9837e856d6db669298.tar.gz";
-  sha256 = "05m4psf3ls7v71nadwzizp12fn2zd7j8svmnibqk3miz7q3i6z07";
+  # url = "https://github.com/nixos/nixpkgs/archive/dad9999266b41da929ca8b9837e856d6db669298.tar.gz";
+  # sha256 = "05m4psf3ls7v71nadwzizp12fn2zd7j8svmnibqk3miz7q3i6z07";
 
   ## Original release
   # url = "https://github.com/nixos/nixpkgs/archive/11cb3517b3af6af300dd6c055aeda73c9bf52c48.tar.gz";
