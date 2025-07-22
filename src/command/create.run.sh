5767df355f5caac7250765196bba6fe6f31e2ac8
Tim Otten <totten@civicrm.org>
2025-07-21 20:22:58 -0700

    civibuild create - Save at midpoint. Facilitate reinstall.

--- src/command/create.run.sh
+++ src/command/create.run.sh
@@ -1,4 +1,9 @@
 civibuild_app_download
+civibuild_app_save
+## Save `build/SITE_NAME.sh` at mid-point. If installer hits a problem, you can do targetted reinstall (without needing to redownload).
+
 civibuild_app_install
 civibuild_app_save
+## Save `build/SITE_NAME.sh` with final data.
+
 civibuild_app_show
